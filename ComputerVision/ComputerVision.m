e75e2026bdf1a35a4dd949bfe599baad667cfa0c
Thijs Vogels <t.vogels@me.com>
2013-03-27 21:03:36 +0100

    Add homogeneous coordinates tools and create some docs

--- ComputerVision/ComputerVision.m
+++ ComputerVision/ComputerVision.m
@@ -1,14 +1,9 @@
 (* Mathematica Package *)
 
-(* Created by the Wolfram Workbench Mar 27, 2013 *)
+BeginPackage["ComputerVision`",{"ComputerVision`HomogeneousCoordinates`"}]
 
-BeginPackage["ComputerVision`"]
-(* Exported symbols added here with SymbolName::usage *) 
 
 Begin["`Private`"]
-(* Implementation of the package *)
-
-
 
 End[]
 
--- PacletInfo.m
+++ PacletInfo.m
@@ -1,11 +1,16 @@
 (* Paclet Info File *)
 
-(* created 2013.03.27*)
+(* created 2013/03/27*)
 
 Paclet[
-  Name -> "ComputerVision",
-  Version -> "0.0.1",
-  MathematicaVersion -> "6+",
-  Extensions -> {
-    {"Documentation", Language -> "English"}
-}]
+    Name -> "ComputerVision",
+    Version -> "0.0.1",
+    MathematicaVersion -> "8+",
+    Creator -> "Thijs Vogels",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English"}
+        }
+]
+
+
